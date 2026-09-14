55100a95e3d54f70ec619b6397ff99699ec30e82
Ayush-raj <rajayush7847@gmail.com>
2026-09-14 23:45:10 +0530

    updated

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-import List from "./pages/list/list";
+import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
 import Login from "./pages/login/Login";
 import MyBookings from "./pages/myBookings/MyBookings";
 import Register from "./pages/register/Register";
-
 import { SearchContextProvider } from "./components/context/searchContext";
 
 function App() {
